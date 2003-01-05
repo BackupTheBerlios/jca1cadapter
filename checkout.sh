16f85b7c414ab26892ef4d7f433c2762631167c5
nio <nio>
2003-01-05 13:19:57 +0000

    *** empty log message ***

--- checkout.sh
+++ checkout.sh
@@ -3,6 +3,7 @@
 #of www.berlios.de using anonymous access
 #
 
+#check if build script is visible
 if [ ! -x "./build.sh" ] ; then
     echo
     echo "No build.sh script found on current path."
@@ -11,14 +12,17 @@ if [ ! -x "./build.sh" ] ; then
     exit
 fi
 
-BUILD_CMD="./build.sh "
+#Defining script commands
+BUILD_CMD="./build.sh"
 CHECKOUT_CMD="checkout"
 CHECKOUTALL_CMD="checkoutall"
 
+#Check if ALL FILES are requested
 if [ "$1" = "all" ] ; then
     $BUILD_CMD $CHECKOUTALL_CMD
 else
     $BUILD_CMD $CHECKOUT_CMD
 fi
 
+#exiting...
 exit
